f70f67906b86f26dab174c67eca1d51bd045d35d
MaximKakoulin <mximm1213@gmail.com>
2022-12-26 17:30:50 +0700

    Первое домашнее задание "Счетчик" Во ViewController написан код, где отображаются кнопки действий и лэйбл текста

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var counter: Int = 0 {
+        didSet {
+            updateCounterLabel()
+        }
+    }
+    @IBOutlet weak var removeButton: UIButton!
+    @IBOutlet weak var appendButton: UIButton!
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var clearButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupAppereance()
     }
-
-
+    
+    @IBAction func buttomAppend(_ sender: Any) {
+        counter += 1
+    }
+    
+    @IBAction func buttomRemove(_ sender: Any) {
+        counter -= 1
+    }
+    
+    @IBAction func buttonClear(_ sender: Any) {
+        counter = 0
+    }
+    
+    private func updateCounterLabel() {
+        counterLabel.text = String("Значеине счетчика: \(counter)")
+    }
+    
+    private func setupAppereance() {
+        appendButton.setTitle("Append", for: .normal)
+        removeButton.setTitle("Remove", for: .normal)
+        counterLabel.text = "Значение счетчика: \(counter)"
+        clearButton.setTitle("Clear", for: .normal)
+    }
+       
 }
+
 
